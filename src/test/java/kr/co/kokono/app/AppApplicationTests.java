ee744fe43fc60ec80e8584ffae1021ff2676fb89
Soon9 <naong526@naver.com>
2020-11-09 20:36:32 +0900

    [20.11.09] #1. Initial commit

---
+++ src/test/java/kr/co/kokono/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kokono.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
